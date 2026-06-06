a3c75dd71922f8a107236af6c56a285432f8c51f
vinayps109 <santoshsingh61340@gmail.com>
2026-06-06 21:14:09 +0530

    updated project

--- first.py
+++ first.py
@@ -5,9 +5,9 @@ import numpy as np
 
 df=pd.read_excel(r"C:\Users\Dell\.vscode\Dataset for Data Anatytics.xlsx")
 df.fillna("NULL",inplace=True)
-#df.drop_duplicates(inplace=True)
-#print("dublicate rows:",df.duplicated().sum())
-#print("null values:",df.isnull().sum())
+df.drop_duplicates(inplace=True)
+print("dublicate rows:",df.duplicated().sum())
+print("null values:",df.isnull().sum())
 df["Date"]=pd.to_datetime(df["Date"])
 df["Date"]=df["Date"].dt.strftime("%d %b %y")
 
